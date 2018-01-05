f7b0589c6cba7b0edc1e8ea6402f4a801e238c6d
nelson <nelsonkenzotamashiro@gmail.com>
2018-01-04 23:04:18 -0200

    finished tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const all_positions = [
       3,4,5,
       6,7,8,  ];
 
-const timeOut_value = 500; // [ms]
+const timeOut_value = 1000; // [ms]
 
 class App extends React.Component{
   render (){
@@ -33,27 +33,30 @@ class Game extends React.Component{
     super();
     this.state = {
       board       : Array(9).fill(null),
-      is_p1Turn : true,
+      is_p1Turn   : true,
+      is_comTurn  : false,
       score : {
         ply1 :0,
         ply2 :0,
       },
     }; // end - state
-    this.update_position  = this.update_position.bind(this);
-    this.change_turn      = this.change_turn.bind(this);
-    this.add_score        = this.add_score.bind(this);
-    this.new_game         = this.new_game.bind(this);
-    this.check_Winner     = this.check_Winner.bind(this);
-    this.check_draw       = this.check_draw.bind(this);
-    this.reset_game       = this.reset_game.bind(this);
-    this.highligh_path    = this.highligh_path.bind(this);
+    this.update_position  = this.update_position.bind (this);
+    this.add_score        = this.add_score.bind       (this);
+    this.change_turn      = this.change_turn.bind     (this);
+    this.check_Winner     = this.check_Winner.bind    (this);
+    this.check_draw       = this.check_draw.bind      (this);
+    this.reset_game       = this.reset_game.bind      (this);
+    this.highligh_path    = this.highligh_path.bind   (this);
+    this.new_game         = this.new_game.bind        (this);
+    this.com_move         = this.com_move.bind        (this);
+    this.usr_move         = this.usr_move.bind        (this);
+    this.game_eval        = this.game_eval.bind       (this);
   } // end - constructor
   
   update_position(pos, callback=()=>{}){
     console.log("update_position called");
     let new_board = this.state.board.slice();
     (this.state.is_p1Turn?new_board[pos]='1':new_board[pos]='2');
-    // this.setState({board: new_board});
     this.setState({board: new_board}, callback);
   } // end - updade board
   
@@ -67,11 +70,15 @@ class Game extends React.Component{
     });
   } // end - add_score
   
-  change_turn(){
+  change_turn(change_comTurn=false, callback=()=>{}){
     console.log('change_turn called');
-    this.setState({
-        is_p1Turn: !this.state.is_p1Turn,
-    });
+    this.setState(
+      {
+        is_p1Turn : !this.state.is_p1Turn,
+        is_comTurn: (change_comTurn?!this.state.is_comTurn:this.state.is_comTurn),
+      },
+      callback
+    );
   } // end - change_turn
   
   check_Winner(){
@@ -100,7 +107,7 @@ class Game extends React.Component{
       {board: Array(9).fill(null)},
       callback
     );
-  }
+  } // end - reset_game
   
   highligh_path(path, callback=()=>{}){
     console.log("highligh_path called");
@@ -110,7 +117,7 @@ class Game extends React.Component{
     }, this);
     this.setState(
       {board: new_board},
-      ()=>{setTimeout(callback,timeOut_value)}
+      callback
     );
   }
   
@@ -122,28 +129,72 @@ class Game extends React.Component{
         ply1: 0,
         ply2: 0,
       },
-    });
+      is_comTurn: false,
+    }, this.change_turn);
   } // end - new_game
+  
+  com_move(callback=()=>{}){
+    console.log("com_move called");
+    console.log()
+    if (this.state.is_comTurn){
+      let possible_moves = all_positions.filter(function(position){
+        if (this.state.board[position]) return false;
+        return true;
+      }, this);
+      let move_choice = parseInt(Math.random()*possible_moves.length, 10);
+      this.update_position( possible_moves[move_choice], callback); // end - update_position
+    } // if - this.state.is_comTurn
+  } // end - com_plays
+  
+  usr_move(pos, callback=()=>{}){
+    console.log("usr_move called");
+    if (!this.state.is_comTurn){
+      this.update_position( pos, callback); // end - update_position
+    } // if - this.state.is_comTurn
+  } // end - com_plays
+  
+  game_eval(callback=()=>{}){ // handles move evaluation - call after every single move
+    console.log("game_eval called");
+    console.log(this.state);
+    let path_num = this.check_Winner();
+    if( path_num !==-1){
+      console.log('winner');
+      this.highligh_path( win_conditions[path_num], ()=>{
+        setTimeout( ()=>{
+          this.add_score();
+          this.reset_game(()=>{
+            this.change_turn(true, callback);
+          });
+          // this.change_turn(true, com_move );
+        },timeOut_value);
+      }); // end - highligh_path
+    } // handles winner
+    else if( this.check_draw() ){
+      console.log('draw');
+      this.highligh_path( all_positions, ()=>{
+        setTimeout(()=>{
+          this.reset_game(()=>{
+            this.change_turn(true, callback);
+          });
+        }, timeOut_value);
+      });
+    } // handles draw
+    else{
+      this.change_turn(true, callback);
+    } // handles game proceed
+  } // end - game_eval
   
   render(){
     let handleClick = (pos)=>{
-      this.update_position(pos, ()=>{
-        let path_num = this.check_Winner();
-        if (path_num !==-1) {
-          this.add_score();
-          this.highligh_path(
-            win_conditions[path_num],
-            this.reset_game,
-          );
-        }
-        else if (this.check_draw()        ) {
-          this.highligh_path(
-            all_positions,
-            this.reset_game
-          );
-        }
-        this.change_turn();
-      });
+      if (!this.state.is_comTurn){
+        this.usr_move(pos, ()=>{
+          this.game_eval(()=>{
+            this.com_move(()=>{
+              this.game_eval();
+            });
+          });
+        }); // end - update_position
+      } // if - is_comTurn
     } // end - handleClick
     return(
       <div className='game flex-col'>
@@ -166,13 +217,12 @@ class GameBoard extends React.Component{
   
   render(){
     let handleClick = this.props.handleClick;
-    let change_turn = this.props.change_turn;
     return(
       <div className='game-board flex-row wrap'>
         {
           this.props.board.map(function(elem, index){
           return (
-            <Button
+            <BoardPosition
               key={'pos'.concat( index.toString() )}
               is_clicked={elem}
               onClick={
@@ -188,7 +238,7 @@ class GameBoard extends React.Component{
   
 } // end - gameBoard
 
-class Button extends React.Component{
+class BoardPosition extends React.Component{
   render(){
     let button_class;
     switch(this.props.is_clicked){
@@ -222,7 +272,7 @@ class GameMenu extends React.Component{
           <div>Player 1</div>
           <div>{this.props.score.ply1}</div>
         </div>
-        <div className='flex-row' onClick={this.props.new_game}>
+        <div className='new-game flex-row' onClick={this.props.new_game}>
           New Game
         </div>
         <div className={ 'flex-col'+(this.props.is_p1Turn?'':' player-turn') }>
